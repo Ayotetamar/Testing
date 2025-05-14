3a51a7e50b2bcfdbc2eaabfb468db8844b06602b
Ayotetamar <ayotetamar@gmail.com>
2025-05-14 11:39:34 +0630

    First Commit

---
+++ src/main/java/com/db_test/Main.java
@@ -0,0 +1,15 @@
+package com.db_test;
+/**
+        This is the testing Main Class. (Document Comment)
+        Created by: ...........
+        Created date: .........
+ */
+public class Main {
+    /**
+        Dot comment
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello and welcome!");
+    }
+}
